17684ff43f001e5cca3e51315d8294aa91412a42
Maria <mariamaciaspadilla@gmail.com>
2020-10-15 10:59:30 +0200

    Agregado spring y adobe XD

---
+++ plataformaEscolar/src/main/java/dam/proyecto/plataformaEscolar/PlataformaEscolarApplication.java
@@ -0,0 +1,13 @@
+package dam.proyecto.plataformaEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaEscolarApplication.class, args);
+	}
+
+}
---
+++ plataformaEscolar/src/test/java/dam/proyecto/plataformaEscolar/PlataformaEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package dam.proyecto.plataformaEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
